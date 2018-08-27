f2ac44e38bc9eb380f6f9cb8ddd95db2a1fd50c7
Kris Shen <ys852@cornell.edu>
2018-08-27 08:41:59 -0400

    edit graph.py

--- HW3/graph/graph.py
+++ HW3/graph/graph.py
@@ -3,43 +3,38 @@ import heapq
 from ast import literal_eval
 
 
-
 def read_file(file):
     """
     Read text file.
     :param file: a .txt file storing graph data
     :return: graph
     """
-    with open(file) as f:
-        text = f.readlines()
-    cnt = 0
-    source = []
-    edges = []
-    for i in range(len(text)):
-        temp = text[i].strip().split("\n")
-        if cnt % 2 == 0:
-            source.append(temp[0])
-        else:
-            edges.append(temp[0])
-        cnt += 1
-    graph = dict([(float(elem), []) for elem in source])
-    next = [[] for i in range(len(source))]
-    for i in range(len(source)):
-        start = 0
-        if edges[i] != "":
-            for j in range(len(edges[i])-1):
-                if edges[i][j] == ")" and edges[i][j + 1] == ",":
-                    next[i].append(literal_eval(edges[i][start: j + 1]))  #
-                    start = j + 2
-            next[i].append(literal_eval(edges[i][start: len(edges[i])]))
-        else:
-            next[i].append("")
-    for i in range(len(source)):
-        for j in range(len(next[i])):
-            if edges[i] != "":
-                temp = (float(next[i][j][0]), float(next[i][j][1]))
-                graph[float(source[i])].append(temp)
-    return(graph)
+    file = open(file)
+    lines = file.readlines()
+    lines = [x.strip() for x in lines]
+    num_nodes = len(lines) / 2
+    graph = dict()
+    for i in range(int(num_nodes)):
+        graph[float(lines[i * 2])] = []
+        edge = []
+        temp = ""
+        if len(lines[i * 2 + 1]) > 0:
+            temp += lines[i * 2 + 1][0]
+        for j in range(1, len(lines[i * 2 + 1])):
+
+            if not (lines[i * 2 + 1][j] == "," and lines[i * 2 + 1][j - 1] == ")"):
+                temp += lines[i * 2 + 1][j]
+            else:
+                temp = literal_eval(temp)
+                temp1 = (float(temp[0]), float(temp[1]))
+                edge.append(temp1)
+                temp = ""
+        if temp != "":
+            temp = literal_eval(temp)
+            temp1 = (float(temp[0]), float(temp[1]))
+            edge.append(temp1)
+        graph[int(lines[i * 2])] = edge
+    return graph
 
 def find_shortest_path(file, source, destination):
     """
@@ -94,7 +89,7 @@ def find_shortest_path(file, source, destination):
     else:
         return path[destination], dist[destination]
 
-def min_path(graph, num_step, source, destination):
+def minpath(graph, num_step, source, destination):
     """
     Find the min path
     :param graph: the graph
@@ -103,39 +98,39 @@ def min_path(graph, num_step, source, destination):
     :param destination: the end point of graph
     :return: the min path
     """
-    if num_step==0:
-        if destination==source:
+    if num_step == 0:
+        if destination == source:
             return 0, [source]
         else:
             return float("inf"), []
-    child=[]
-    path=[]
+    child = []
+    path = []
     for node in graph[source]:
-        temp = min_path(graph, num_step-1, node[0], destination)
+        temp = minpath(graph, num_step - 1, node[0], destination)
         child.append(temp[0] + node[1])
         path.append(temp[1])
-    if len(child)==0:
-        if source==destination:
+    if len(child) == 0:
+        if source == destination:
             return 0, [source]
         else:
             return float("inf"), []
-    temp_min=np.min(child)
-    min_path=path[np.argmin(child)][:]
-    previous=min_path(graph, num_step-1, source, destination)
-    minimum = np.min([temp_min,previous[0]])
-    if temp_min<previous[0]:
-        min_path.insert(0,source)
+    temp_min = np.min(child)
+    min_path = path[np.argmin(child)][:]
+    previous = minpath(graph, num_step - 1, source, destination)
+    minimum = np.min([temp_min, previous[0]])
+    if temp_min < previous[0]:
+        min_path.insert(0, source)
     else:
-        min_path=previous[1][:]
+        min_path = previous[1][:]
     return minimum, min_path
 
-def find_negative_cicles(name_txt_file):
+def find_negative_cycles(name_txt_file):
     graph = read_file(name_txt_file)
     N=len(graph)
     for source,source_child in graph.items():
         for destination, destination_child in graph.items():
-            temp_N=min_path(graph, N, source, destination)
-            temp_n=min_path(graph, N-1, source, destination)
+            temp_N=minpath(graph, N, source, destination)
+            temp_n=minpath(graph, N-1, source, destination)
             path=[]
             if temp_N[0]<temp_n[0]:
                 path=temp_N[1][:]
